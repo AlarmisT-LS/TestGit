056341c709c6daea4e06658918b8ee7a9980e5d7
Nikita <alarmistgg@gmail.com>
2020-10-29 05:25:11 +0300

    Change to enter first and last name

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -6,9 +6,11 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите своё имя:");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Привет {name}!");
+            Console.WriteLine("Введите своё имя и фамилию:");
+            (string name, string surname) person;
+            Console.Write("Введите имя:"); person.name = Console.ReadLine();
+            Console.Write("Введите фамилию:"); person.surname = Console.ReadLine();
+            Console.WriteLine($"Привет {person.name} {person.surname}!");
         }
     }
 }
