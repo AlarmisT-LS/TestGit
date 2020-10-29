2276a7d3b224a75ba1eed4255030859d79d4612b
Nikita <alarmistgg@gmail.com>
2020-10-29 05:26:48 +0300

    Age input

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -7,10 +7,12 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("Введите своё имя и фамилию:");
-            (string name, string surname) person;
+            (string name, string surname, int age) person;
             Console.Write("Введите имя:"); person.name = Console.ReadLine();
             Console.Write("Введите фамилию:"); person.surname = Console.ReadLine();
             Console.WriteLine($"Привет {person.name} {person.surname}!");
+            Console.WriteLine($"Сколько тебе лет {person.name}?");
+            Console.Write("Возраст:"); person.age = int.Parse(Console.ReadLine());
         }
     }
 }
